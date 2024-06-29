f013b3c5e7fe673d682d6416b25de867df716816
sabari-qwerty <sabarigithub@gmail.com>
2024-06-29 17:36:56 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleSend = () => {
+    window.parent.postMessage("hello sabri", " http://localhost:5173/");
+  };
+
+  return (
+    <>
+      <button onClick={() => handleSend()}>send message</button>
+    </>
+  );
+}
+
+export default App;
