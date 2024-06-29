c95f4d1a01658a31b35e8dd1b85916a941928d70
sabari-qwerty <sabarigithub@gmail.com>
2024-06-29 17:39:34 +0530

    removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   const handleSend = () => {
     window.parent.postMessage("hello sabri", " http://localhost:5173/");
   };
